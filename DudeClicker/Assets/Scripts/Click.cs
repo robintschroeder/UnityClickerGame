359c91d1f8ab20d8a0f81c4ec4e627bbf3a6c001
robin@msctek.com <robin@msctek.com>
2018-08-19 20:22:41 -0500

    initial check-in, must admit, not exactly sure what goes into source control in a unity soln ;-p

---
+++ DudeClicker/Assets/Scripts/Click.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+    public UnityEngine.UI.Text goldDisplay;
+    public int gold = 0;
+    public int goldperclick = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        goldDisplay.text = "Gold: " + gold;
+	}
+
+    public void Clicked()
+    {
+        gold = gold + goldperclick;
+    }
+
+    
+
+    public void OnClick()
+    {
+        gold = gold + goldperclick;
+    }
+}
