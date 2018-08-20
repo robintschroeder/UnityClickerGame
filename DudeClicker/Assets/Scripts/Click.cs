bb96bf9d0052fe0f2187aa1d1d0468308bce775f
robin@msctek.com <robin@msctek.com>
2018-08-19 21:01:52 -0500

    added UpgradeManager and a few new buttons

--- DudeClicker/Assets/Scripts/Click.cs
+++ DudeClicker/Assets/Scripts/Click.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class Click : MonoBehaviour {
 
     public UnityEngine.UI.Text goldDisplay;
-    public int gold = 0;
+    public float gold = 0;
     public int goldperclick = 1;
+    public UnityEngine.UI.Text gpc;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,7 @@ public class Click : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         goldDisplay.text = "Gold: " + gold;
+        gpc.text = "GPC: " + goldperclick;
 	}
 
     public void Clicked()
---
+++ DudeClicker/Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour {
+
+    public Click click;
+    public UnityEngine.UI.Text itemInfo;
+    public float cost;
+    public int count = 0;
+
+    public int clickPower;
+    public string itemName;
+    private float _newCost;
+
+    void Update()
+    {
+        itemInfo.text = itemName + "\nCost: " + cost + "\nPower: +" + clickPower;
+    }
+
+    public void PurchaseUpgrade()
+    {
+        if(click.gold >= cost)
+        {
+            click.gold -= cost;
+            count += 1;
+            click.goldperclick += clickPower;
+            cost = Mathf.Round(cost * 1.15f);
+            _newCost = Mathf.Pow(cost, _newCost = cost);
+        }
+    }
+	
+}
